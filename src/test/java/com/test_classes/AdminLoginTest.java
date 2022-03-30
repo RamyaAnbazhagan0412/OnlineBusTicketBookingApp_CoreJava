51ea68b7cee472cde3b58fa78a00d4236d492346
RamyaAnbazhagan0412 <aramya0412@gmail.com>
2022-03-30 14:54:30 +0530

    added features

---
+++ src/test/java/com/test_classes/AdminLoginTest.java
@@ -0,0 +1,15 @@
+package com.test_classes;
+
+import java.sql.SQLException;
+
+import com.model_classes.AdminLogin;
+
+public class AdminLoginTest {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		//1. calling  the admin class.method
+		
+		AdminLogin.adminLogin();
+	}
+
+}
